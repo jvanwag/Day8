63b8c350a45f27da5d5fe0e3db018bf79dbe7ae6
Jon Van Wagenen <jvanwag08@gmail.com>
2016-02-10 15:11:46 -0700

    finished the project

---
+++ callbackPractice.js
@@ -0,0 +1,195 @@
+/* In this repo your job is to write functions to make each function call work properly.
+Below is a sample problem
+
+  //code here for sayHi
+
+   sayHi('Hi Katie', function(thingToSay){
+      alert(thingToSay);
+   });
+
+
+and what you should write is the sayHi function that makes the code above work,
+
+
+   var sayHi = function(str, cb){
+    cb(str);
+   }
+
+   sayHi('Hi Katie', function(thingToSay){
+      alert(thingToSay); //should alert ('Hi Katie')'
+   });
+
+
+*/
+
+
+
+var first = function(arr, getName) {
+  x = arr[0];
+  getName(x);
+} //Code Here for first
+
+
+var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
+first(names, function(firstName){
+  console.log('The first name in names is ' + firstName)
+});
+
+
+
+/* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
+
+
+
+
+var last = function(arr, anotherName) {
+  x = arr[arr.length-1];
+  anotherName(x);
+}  //Code Here for last
+
+var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
+last(names, function(lastName){
+  console.log('The last name in names is ' + lastName);
+});
+
+
+
+
+
+/* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
+
+
+
+
+
+
+var multiply = function(num1, num2, numMul) {
+  x = num1 * num2;
+  numMul(x);
+}  //Code Here for multiply
+
+multiply(4, 3, function(answer){
+  console.log('The answer is ' + answer); //should console.log 12
+})
+
+
+
+
+
+/* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
+
+
+
+
+
+var contains = function(arr, str, func1) {
+  x = false
+  for(var i = 0; i < arr.length-1;i++) {
+    if (arr[i] === str) {
+      x = true;
+    }
+  }
+  func1(x);
+}  //Code Here for contains
+
+var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
+contains(names, 'Colt', function(result){
+  if(result === true){
+    console.log('Colt is in the array');
+  } else {
+    console.log('Colt is not in the array');
+  }
+});
+
+
+
+
+
+/* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
+
+
+
+
+var uniq = function(arr, ans) {
+  var newAry = [];
+  var obj = {};
+  for(var i = 0; i < arr.length; i++) {
+    var num = arr[i];
+    if (!obj [num]) {
+      newAry.push(num);
+      obj[num]= true;
+    }
+
+  }
+  ans(newAry);
+}
+
+var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
+uniq(names, function(uniqArr){
+  console.log('The new names array with all the duplicate items removed is ', uniqArr);
+});
+
+
+
+
+
+/* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
+
+
+
+var each = function(arr, getIndex) {
+  for (var i = 0; i < arr.length; i++) {
+  var item = arr[i];
+  var index = i;
+  getIndex(item, index);
+}
+}    //Code Here for each
+
+var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
+each(names, function(item, indice){
+  console.log('The item in the ' + indice + ' position is ' + item)
+});
+
+
+
+
+
+/* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
+
+
+
+
+
+ var getUserById = function(arr, prop, getInfo) {
+   for(var i = 0; i < arr.length; i++) {
+     if(arr[i].id === prop) {
+       var user = arr[i];
+       getInfo(user);
+     }
+   }
+ }//code here for getUserById
+
+var users = [
+  {
+    id: '12d',
+    email: 'tyler@gmail.com',
+    name: 'Tyler',
+    address: '167 East 500 North'
+  },
+  {
+    id: '15a',
+    email: 'cahlan@gmail.com',
+    name: 'Cahlan',
+    address: '135 East 320 North'
+  },
+  {
+    id: '16t',
+    email: 'ryan@gmail.com',
+    name: 'Ryan',
+    address: '192 East 32 North'
+  },
+];
+
+getUserById(users, '16t', function(user){
+  console.log('The user with the id 16t has the email of ' + user.email + ' the name of ' + user.name + ' and the address of ' + user.address);
+});
